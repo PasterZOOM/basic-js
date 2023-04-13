6c33535bbebeb3d4b9686fcb2c63f20628c1fb1b
ilya <pasterzoom@gmail.com>
2023-04-14 01:07:31 +0300

    feat: make simple chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,32 +1,42 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
 
 /**
  * Implement chainMaker object according to task description
- * 
+ *
  */
 const chainMaker = {
-  getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    _links: '',
+    getLength() {
+        return length
+    },
+    addLink(value) {
+        this._links = this._links ? `${this._links}~~( ${value} )` : `( ${value} )`
+        return this
+    },
+    removeLink(position) {
+        if (this._links.split('~~').length <= position
+            || position < 1
+            || typeof position !== "number"
+            || Math.floor(position) !== position) {
+            this._links = ''
+            throw new Error('You can\'t remove incorrect link!')
+        } else {
+            this._links = this._links.split('~~').filter((_, index) => index + 1 !== position).join('~~')
+            return this
+        }
+
+    },
+    reverseChain() {
+        this._links = this._links.split('~~').reverse().join('~~')
+        return this
+    },
+    finishChain() {
+        const res = this._links
+        this._links = ''
+        return res
+    }
 };
 
 module.exports = {
-  chainMaker
+    chainMaker
 };
